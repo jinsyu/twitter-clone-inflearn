dd4c4bacc75cc0dc0be67172a0ae44c0f0411f30
jinsyu <jinsyu.com@gmail.com>
2021-02-12 03:22:56 +0900

    1-14

--- src/App.vue
+++ src/App.vue
@@ -2,87 +2,87 @@
   <div class="flex h-screen container mx-auto">
     <!-- side section -->
     <div
-      class="w-20 lg:w-1/4 pt-5 lg:ml-10 flex flex-col justify-between border-r border-gray-100"
+      class="w-20 xl:w-1/4 pt-5 xl:ml-10 flex flex-col justify-between border-r border-gray-100"
     >
-      <div class="flex flex-col items-center lg:items-start">
+      <div class="flex flex-col items-center xl:items-start">
         <!-- twitter logo -->
-        <i class="fab fa-twitter text-3xl text-primary lg:ml-4 mb-3"></i>
+        <i class="fab fa-twitter text-3xl text-primary xl:ml-4 mb-3"></i>
         <!-- sidemenu icons -->
         <div class="flex flex-col items-start space-y-1">
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="fas fa-home fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">홈</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="fas fa-hashtag fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">탐색하기</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-bell fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">알림</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-envelope fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">쪽지</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-bookmark fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">북마크</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-list-alt fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">리스트</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-user fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">프로필</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
-            <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
+            <span class="ml-5 text-xl hidden xl:inline-block">더보기</span>
           </div>
         </div>
         <!-- tweet button -->
-        <div class="w-full lg:pr-3 flex justify-center">
+        <div class="w-full xl:pr-3 flex justify-center">
           <button
-            class="mt-3 bg-primary text-white lg:w-full w-12 h-12 rounded-full hover:bg-dark"
+            class="mt-3 bg-primary text-white xl:w-full w-12 h-12 rounded-full hover:bg-dark"
           >
-            <span class="hidden lg:block">트윗</span>
-            <i class="fas fa-plus lg:hidden"></i>
+            <span class="hidden xl:block">트윗</span>
+            <i class="fas fa-plus xl:hidden"></i>
           </button>
         </div>
       </div>
       <!-- profile button -->
-      <div class="lg:pr-3 mb-3 relative">
+      <div class="xl:pr-3 mb-3 relative">
         <button
-          class="hidden lg:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 items-center"
+          class="hidden xl:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 items-center"
         >
           <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full" />
-          <div class="lg:ml-2 hidden lg:block">
+          <div class="xl:ml-2 hidden xl:block">
             <div class="text-sm font-bold">jinsyu.com</div>
             <div class="text-xs text-gray-500 text-left">@jinsyu</div>
           </div>
           <i
-            class="ml-auto fas fa-ellipsis-h fa-fw text-xs hidden lg:block"
+            class="ml-auto fas fa-ellipsis-h fa-fw text-xs hidden xl:block"
           ></i>
         </button>
-        <div class="lg:hidden flex justify-center">
+        <div class="xl:hidden flex justify-center">
           <img
             src="http://picsum.photos/100"
             class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80"
@@ -92,15 +92,15 @@
     </div>
     <!-- main section -->
     <div class="flex-1 flex h-screen">
-      <Messages />
+      <Profile />
     </div>
   </div>
 </template>
 
 <script>
-import Messages from './pages/Messages.vue'
+import Profile from './pages/Profile.vue'
 export default {
-  components: { Messages },
+  components: { Profile },
   setup() {},
 }
 </script>
---
+++ src/pages/Profile.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="flex-1 flex">
+    <!-- profile section -->
+    <div class="flex-1 flex flex-col border-r border-color">
+      <!-- title -->
+      <div class="px-3 py-1 flex border-b border-color">
+        <button class="mr-4">
+          <i
+            class="fas fa-arrow-left text-primary p-3 rounded-full hover:bg-blue-50"
+          ></i>
+        </button>
+        <div>
+          <div class="font-extrabold text-lg">jinsyu.com</div>
+          <div class="text-xs text-gray">393 트윗</div>
+        </div>
+      </div>
+      <!-- background image -->
+      <div class="bg-gray-300 h-40 relative">
+        <!-- profile image -->
+        <div
+          class="border-4 border-white bg-gray-100 w-28 h-28 rounded-full absolute -bottom-14 left-2"
+        >
+          <img
+            src="http://picsum.photos/200"
+            class="rounded-full opacity-90 hover:opacity-100 cursor-pointer"
+          />
+        </div>
+      </div>
+      <!-- profile edit button -->
+      <div class="text-right mt-2 mr-2">
+        <button
+          class="border text-sm border-primary text-primary px-3 py-2 hover:bg-blue-50 font-bold rounded-full"
+        >
+          프로필 수정
+        </button>
+      </div>
+      <!-- user info -->
+      <div class="mx-3 mt-2">
+        <div class="font-extrabold text-lg">jinsyu.com</div>
+        <div class="text-gray">@jinsyu</div>
+        <div>
+          <span class="text-gray">가입일:</span>
+          <span class="text-gray">2011년 10월</span>
+        </div>
+        <div>
+          <span class="font-bold mr-1">28</span>
+          <span class="text-gray mr-3">팔로우 중</span>
+          <span class="font-bold mr-1">7</span>
+          <span class="text-gray">팔로워</span>
+        </div>
+      </div>
+      <!-- tabs -->
+      <div class="flex border-b border-color mt-3">
+        <div
+          class="py-3 text-primary font-bold border-b border-primary text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+        <div
+          class="py-3 text-gray font-bold text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+        <div
+          class="py-3 text-gray font-bold text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+        <div
+          class="py-3 text-gray font-bold text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+      </div>
+    </div>
+    <!-- trend section -->
+    <Trends />
+  </div>
+</template>
+
+<script>
+import Trends from '../components/Trends.vue'
+export default {
+  components: { Trends },
+}
+</script>
+
+<style></style>
