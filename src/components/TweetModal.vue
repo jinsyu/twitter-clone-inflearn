e2e26de827c617c7d6c65e50b737c1a47f3beb28
jinsyu <jinsyu.com@gmail.com>
2021-02-18 01:44:23 +0900

    2-17

---
+++ src/components/TweetModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="fixed z-10 inset-0 overflow-y-auto" @click="$emit('close-modal')">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+      </div>
+
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+      <!-- contents -->
+      <div @click.stop class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+        <div class="border-b border-gray-100 p-2">
+          <button class="fas fa-times text-primary text-lg p-2 h-10 w-10 hover:bg-blue-50 rounded-full"></button>
+        </div>
+        <!-- tweeting section -->
+        <div class="flex p-4">
+          <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
+          <div class="ml-2 flex-1 flex flex-col">
+            <textarea v-model="tweetBody" rows="5" placeholder="무슨 일이 일어나고 있나요?" class="w-full text-lg font-bold focus:outline-none mb-3 resize-none"></textarea>
+            <div class="text-right">
+              <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+              <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const tweetBody = ref('')
+
+    return {
+      tweetBody,
+    }
+  },
+}
+</script>
+
+<style></style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -16,7 +16,7 @@
         </div>
         <!-- tweet button -->
         <div class="w-full xl:pr-3 flex justify-center">
-          <button class="mt-3 bg-primary text-white xl:w-full w-12 h-12 rounded-full hover:bg-dark">
+          <button @click="showTweetModal = true" class="mt-3 bg-primary text-white xl:w-full w-12 h-12 rounded-full hover:bg-dark">
             <span class="hidden xl:block">트윗</span>
             <i class="fas fa-plus xl:hidden"></i>
           </button>
@@ -53,6 +53,9 @@
       </button>
       <button class="p-3 hover:bg-gray-50 w-full text-left text-sm" @click="onLogout">@{{ currentUser.username }} 계정에서 로그아웃</button>
     </div>
+
+    <!-- tweet modal popup -->
+    <tweet-modal v-if="showTweetModal" @close-modal="showTweetModal = false"></tweet-modal>
   </div>
 </template>
 
@@ -61,11 +64,14 @@ import { ref, onBeforeMount, computed } from 'vue'
 import router from '../router'
 import { auth } from '../firebase'
 import store from '../store'
+import TweetModal from '../components/TweetModal.vue'
 
 export default {
+  components: { TweetModal },
   setup() {
     const routes = ref([])
     const showProfileDropdown = ref(false)
+    const showTweetModal = ref(false)
 
     const currentUser = computed(() => store.state.user)
 
@@ -79,7 +85,7 @@ export default {
       routes.value = router.options.routes.filter((route) => route.meta.isMenu == true)
     })
 
-    return { routes, showProfileDropdown, onLogout, currentUser, router }
+    return { routes, showProfileDropdown, onLogout, currentUser, router, showTweetModal }
   },
 }
 </script>
