8d2f9e79b1398f603808c30aec2b7429e03c6cd4
jinsyu <jinsyu.com@gmail.com>
2021-02-19 05:49:32 +0900

    2-18-2

--- src/components/TweetModal.vue
+++ src/components/TweetModal.vue
@@ -7,15 +7,21 @@
 
       <!-- contents -->
       <div @click.stop class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-        <div class="border-b border-gray-100 p-2">
+        <div class="border-b border-gray-100 p-2 flex items-center justify-between">
           <button @click="$emit('close-modal')" class="fas fa-times text-primary text-lg p-2 h-10 w-10 hover:bg-blue-50 rounded-full"></button>
+          <!-- tweet button -->
+          <div class="text-right sm:hidden mr-2">
+            <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+            <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
+          </div>
         </div>
         <!-- tweeting section -->
         <div class="flex p-4">
           <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
           <div class="ml-2 flex-1 flex flex-col">
             <textarea v-model="tweetBody" rows="5" placeholder="무슨 일이 일어나고 있나요?" class="w-full text-lg font-bold focus:outline-none mb-3 resize-none"></textarea>
-            <div class="text-right">
+            <!-- tweet button -->
+            <div class="text-right hidden sm:block">
               <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
               <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
             </div>
