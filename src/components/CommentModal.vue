74a1dc995a7d638daed736037e6190e33d94056d
jinsyu <jinsyu.com@gmail.com>
2021-02-19 07:19:37 +0900

    2-19

---
+++ src/components/CommentModal.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="fixed z-10 inset-0 overflow-y-auto" @click="$emit('close-modal')">
+    <div class="flex justify-center min-h-screen sm:pt-6 sm:px-4 sm:pb-20 text-center sm:block sm:p-0">
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+      </div>
+      <!-- contents -->
+      <div @click.stop class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+        <div class="border-b border-gray-100 p-2 flex items-center justify-between">
+          <button @click="$emit('close-modal')" class="fas fa-times text-primary text-lg p-2 h-10 w-10 hover:bg-blue-50 rounded-full"></button>
+          <!-- tweet button -->
+          <div class="text-right sm:hidden mr-2">
+            <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">답글</button>
+            <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">답글</button>
+          </div>
+        </div>
+        <div>
+          <!-- original tweet -->
+          <div class="flex px-4 pt-4 pb-3">
+            <div class="flex flex-col">
+              <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
+              <div class="ml-5 w-0.5 h-full bg-gray-300 mt-2 -mb-1"></div>
+            </div>
+            <div class="ml-2 flex-1">
+              <div class="flex space-x-2">
+                <span class="font-bold text-sm">Bernie Sanders</span>
+                <span class="text-gray text-sm">@BernieSanders</span>
+                <span class="text-gray text-sm">1시간</span>
+              </div>
+              <div class="text-sm">When the gap between the very rich & everyone else is widening; when the 50 richest Americans own more wealth than the bottom half of our country; when the effective tax rate of billionaires is less than that of average workers; YES. We must address income & wealth inequality.</div>
+              <div>
+                <span class="text-primary text-sm">@JoeBiden </span>
+                <span class="text-gray text-sm"> 님에게 보내는 답글</span>
+              </div>
+            </div>
+          </div>
+          <!-- tweeting section -->
+          <div class="flex px-4 pb-4">
+            <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
+            <div class="ml-2 flex-1 flex flex-col">
+              <textarea v-model="tweetBody" rows="5" placeholder="내 답글을 트윗합니다" class="w-full text-lg font-bold focus:outline-none mb-3 resize-none"></textarea>
+              <!-- tweet button -->
+              <div class="text-right hidden sm:block">
+                <button v-if="!tweetBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">답글</button>
+                <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">답글</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const tweetBody = ref('')
+
+    return {
+      tweetBody,
+    }
+  },
+}
+</script>
+
+<style></style>
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -13,33 +13,43 @@
       <div>{{ tweet.tweet_body }}</div>
       <!-- tweet actions -->
       <div class="flex justify-between">
-        <div class="text-gray-500 hover:text-primary">
+        <!-- comment button -->
+        <div @click="showCommentModal = true" class="text-gray-500 hover:text-primary">
           <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
           <span class="ml-1 text-sm">{{ tweet.num_comments }}</span>
         </div>
+        <!-- retweet button -->
         <div class="text-gray-500 hover:text-green-500">
           <i class="fas fa-retweet hover:bg-green-50 rounded-full p-2"></i>
           <span class="ml-1 text-sm">{{ tweet.num_retweets }}</span>
         </div>
+        <!-- like button -->
         <div class="text-gray-500 hover:text-red-500">
           <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
           <span class="ml-1 text-sm">{{ tweet.num_likes }}</span>
         </div>
+        <!-- share button -->
         <div class="text-gray-500 hover:text-primary">
           <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
         </div>
       </div>
     </div>
+    <comment-modal v-if="showCommentModal" @close-modal="showCommentModal = false"></comment-modal>
   </div>
 </template>
 
 <script>
 import moment from 'moment'
+import { ref } from 'vue'
+import CommentModal from './CommentModal.vue'
 export default {
+  components: { CommentModal },
   props: ['currentUser', 'tweet'],
   setup() {
+    const showCommentModal = ref(false)
     return {
       moment,
+      showCommentModal,
     }
   },
 }
